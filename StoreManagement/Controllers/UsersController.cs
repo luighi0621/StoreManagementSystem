60d158ff2633e429cb3ca69b1ff2462e1f8c012e
Luis Choque Castro <luis.choque@avantica.net>
2018-06-04 17:05:36 -0400

    Details for users working

--- StoreManagement/Controllers/UsersController.cs
+++ StoreManagement/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using StoreManagement.Dal.Interfaces;
 using StoreManagement.Dal;
 using StoreManagement.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace StoreManagement.Controllers
 {
@@ -54,6 +55,58 @@ namespace StoreManagement.Controllers
                 return NotFound();
             }
             return View(user);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.GetAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Firstname,Lastname,Login,Password")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Get(e => e.Id == id) != null;
         }
     }
 }
